6c1e43eb5803ec6d2864dc4b123ee4b264dfad0b
preranathapamagar19@gmail.com <preranathapamagr19@gmail.com>
2026-09-07 22:56:07 +0545

    react-0

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 // console.log(React);
-// console.log(people);
 
 let h1 = document.createElement("h1");
-h1.textContent = "Hello";
+h1.textContent ="Hello";
 document.body.append(h1);
 
 let rh1 = React.createElement("h1", null, "hello i m form react");
+
+//props- argument that we can passs(attributes, properties)
 
 console.log("real dom->", h1);
 console.log("real dom->", rh1);
