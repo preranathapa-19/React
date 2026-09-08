c477dd75e5a7fe14d7bf6ebefd5094ad0d075d97
preranathapamagar19@gmail.com <preranathapamagr19@gmail.com>
2026-09-08 10:53:11 +0545

    how reactDOM work

--- script.js
+++ script.js
@@ -1,12 +1,22 @@
 // console.log(React);
 
+
+//Real DOM 
 let h1 = document.createElement("h1");
 h1.textContent ="Hello";
 document.body.append(h1);
 
-let rh1 = React.createElement("h1", null, "hello i m form react");
+
+//ReactDOM, virtual DOM
+let rh1 = React.createElement(
+    "h1",
+     null, 
+    React.createElement("span", {}, "i am under h1"));
 
 //props- argument that we can passs(attributes, properties)
 
-console.log("real dom->", h1);
-console.log("real dom->", rh1);
+let realDomElem = document.querySelector("#root"); //Select here
+
+let rootOfReact = ReactDOM.createRoot(realDomElem); //react create it own dom
+
+rootOfReact.render(rh1);
