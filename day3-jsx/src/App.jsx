9509c29dae9497df3155c56ccbfc30a0e23bead4
preranathapamagar19@gmail.com <preranamagar19@gmail.com>
2026-09-13 09:27:35 +0545

    refactor: update App component structure and remove unused CSS file

--- day3-jsx/src/App.jsx
+++ day3-jsx/src/App.jsx
@@ -1,12 +1,15 @@
-import React from "react"; 
+import React from "react";
+import About from "./about";
 
-let App = () => {
-  let ui = React.createElement("div",{},[
-    React.createElement("h1",{},"hello"),
-      React.createElement("h2",{},"Go hi"),
-      React.createElement("h3",{},"Go Back")
-    ]);
-      return ui;
+let App = () =>{
+  return (
+    <div>
+      <h1>HEllo</h1>
+      <h2>bye</h2>
+      <h3>Go back</h3>
+      <About />
+    </div>
+  );
 };
 
 export default App;
