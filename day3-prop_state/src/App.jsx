952dfb3322aa3c9b9b44cddfa7a05ef88cfbb913
preranathapamagar19@gmail.com <preranamagar19@gmail.com>
2026-09-14 22:00:09 +0545

    Remove unused assets and styles, including icons, images, and CSS files, to clean up the project structure.

---
+++ day3-prop_state/src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Hero from './components/Hero'
+import Navbar from "./Navbar";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <div>
+
+
+    <Navbar />
+    <Hero />
+    <Footer />
+    </div>
+  )
+}
+
+export default App
